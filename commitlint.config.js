0e14a634966a24b9bcfc66ee1c05d4b8cb634655
matheusgondra <matheusgondra951@gmail.com>
2024-04-07 11:51:59 -0300

    chore: add commit linter, husky and lint-staged

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	extends: [
+		"@commitlint/config-conventional"
+	],
+	rules: {
+		"type-enum": [
+			2,
+			"always",
+			[
+				"build",
+				"ci",
+				"chore",
+				"docs",
+				"feat",
+				"fix",
+				"hotfix",
+				"perf",
+				"refactor",
+				"revert",
+				"style",
+				"test"
+			]
+		]
+	}
+}
