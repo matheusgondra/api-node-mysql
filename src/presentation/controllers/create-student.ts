ff6ae4f2d119aa4580cd90bf80674c43781620ef
matheusgondra <matheusgondra951@gmail.com>
2024-04-07 14:01:41 -0300

    feat: ensure CreateStudentController return 400 if Validation returns an error

--- src/presentation/controllers/create-student.ts
+++ src/presentation/controllers/create-student.ts
@@ -1,10 +1,14 @@
+import { badRequest } from "../helpers/http";
 import { Controller, HttpRequest, HttpResponse, Validation } from "../protocols";
 
 export class CreateStudentController implements Controller {
 	constructor(private readonly validation: Validation) {}
 
 	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-		this.validation.validate(httpRequest.body);
+		const error = this.validation.validate(httpRequest.body);
+		if (error) {
+			return badRequest(error);
+		}
 		return {
 			statusCode: 201,
 			body: null
---
+++ src/presentation/helpers/http.ts
@@ -0,0 +1,6 @@
+import { HttpResponse } from "../protocols";
+
+export const badRequest = (error: Error): HttpResponse => ({
+	statusCode: 400,
+	body: error
+});
