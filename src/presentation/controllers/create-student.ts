0ae35b567c841da698455fb7902979f71829346a
matheusgondra <matheusgondra951@gmail.com>
2024-04-07 21:34:37 -0300

    feat: ensure CreateStudentController return 500 if AddStudent throws

--- src/presentation/controllers/create-student.ts
+++ src/presentation/controllers/create-student.ts
@@ -1,5 +1,5 @@
 import { AddStudent } from "../../domain/use-cases/add-student";
-import { badRequest } from "../helpers/http";
+import { badRequest, serverError } from "../helpers/http";
 import { Controller, HttpRequest, HttpResponse, Validation } from "../protocols";
 
 export class CreateStudentController implements Controller {
@@ -9,16 +9,20 @@ export class CreateStudentController implements Controller {
 	) {}
 
 	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-		const error = this.validation.validate(httpRequest.body);
-		if (error) {
-			return badRequest(error);
+		try {
+			const error = this.validation.validate(httpRequest.body);
+			if (error) {
+				return badRequest(error);
+			}
+
+			await this.addStudent.add(httpRequest.body);
+
+			return {
+				statusCode: 201,
+				body: null
+			};
+		} catch (error) {
+			return serverError(error as Error);
 		}
-
-		this.addStudent.add(httpRequest.body);
-
-		return {
-			statusCode: 201,
-			body: null
-		};
 	}
 }
--- src/presentation/helpers/http.ts
+++ src/presentation/helpers/http.ts
@@ -1,6 +1,12 @@
+import { ServerError } from "../errors/server-error";
 import { HttpResponse } from "../protocols";
 
 export const badRequest = (error: Error): HttpResponse => ({
 	statusCode: 400,
 	body: error
+});
+
+export const serverError = (error: Error): HttpResponse => ({
+	statusCode: 500,
+	body: new ServerError(error.stack)
 });
