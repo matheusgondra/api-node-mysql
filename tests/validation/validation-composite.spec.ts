01348796480b37793f9a56e8b3748885c4fdc3b4
matheusgondra <matheusgondra951@gmail.com>
2024-04-15 22:34:23 -0300

    refactor: move sut creation to a helper factory method

--- tests/validation/validation-composite.spec.ts
+++ tests/validation/validation-composite.spec.ts
@@ -1,15 +1,24 @@
 import { Validation } from "../../src/presentation/protocols";
 import { ValidationComposite } from "../../src/validation";
 
+const mekeValidationStub = (): Validation => {
+	class ValidationStub implements Validation {
+		validate(input: any): Error | null {
+			return null;
+		}
+	}
+	return new ValidationStub();
+};
+
+const makeSut = (): ValidationComposite => {
+	const validationStubs = [mekeValidationStub(), mekeValidationStub()];
+	const sut = new ValidationComposite(validationStubs);
+	return sut;
+};
+
 describe("ValidationComposite", () => {
 	it("Should return null if validation succeeds", () => {
-		class ValidationStub implements Validation {
-			validate(input: any): Error | null {
-				return null;
-			}
-		}
-		const validations: Validation[] = [new ValidationStub()];
-		const sut = new ValidationComposite(validations);
+		const sut = makeSut();
 		const error = sut.validate({ name: "any_name" });
 		expect(error).toBeNull();
 	});
