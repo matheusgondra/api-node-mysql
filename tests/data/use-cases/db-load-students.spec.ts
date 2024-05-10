031544ae7ac4db1d01fa737269d3b9153df73c7d
matheusgondra <matheusgondra951@gmail.com>
2024-05-10 20:28:39 -0300

    refactor: move sut creation to a factory method

--- tests/data/use-cases/db-load-students.spec.ts
+++ tests/data/use-cases/db-load-students.spec.ts
@@ -2,28 +2,45 @@ import { StudentModel } from "../../../src/domain/models";
 import { LoadStudentsRepository } from "../../../src/data/protocols";
 import { DbLoadStudents } from "../../../src/data/use-cases";
 
+const makeLoadStudentsRepositoryStub = (): LoadStudentsRepository => {
+	class LoadStudentsRepositoryStub implements LoadStudentsRepository {
+		async load(): Promise<StudentModel[]> {
+			return [
+				{
+					id: 1,
+					name: "any_name",
+					cpf: "any_cpf",
+					responsible: "any_responsible"
+				},
+				{
+					id: 2,
+					name: "any_name",
+					cpf: "any_cpf",
+					responsible: "any_responsible"
+				}
+			];
+		}
+	}
+	return new LoadStudentsRepositoryStub();
+};
+
+interface SutTypes {
+	sut: DbLoadStudents;
+	loadStudentsRepositoryStub: LoadStudentsRepository;
+}
+
+const makeSut = (): SutTypes => {
+	const loadStudentsRepositoryStub = makeLoadStudentsRepositoryStub();
+	const sut = new DbLoadStudents(loadStudentsRepositoryStub);
+	return {
+		sut,
+		loadStudentsRepositoryStub
+	};
+};
+
 describe("DbLoadStudents", () => {
 	it("Should call LoadStudentsRepository with correct values", async () => {
-		class LoadStudentsRepositoryStub implements LoadStudentsRepository {
-			async load(): Promise<StudentModel[]> {
-				return [
-					{
-						id: 1,
-						name: "any_name",
-						cpf: "any_cpf",
-						responsible: "any_responsible"
-					},
-					{
-						id: 2,
-						name: "any_name",
-						cpf: "any_cpf",
-						responsible: "any_responsible"
-					}
-				];
-			}
-		}
-		const loadStudentsRepositoryStub = new LoadStudentsRepositoryStub();
-		const sut = new DbLoadStudents(loadStudentsRepositoryStub);
+		const { sut, loadStudentsRepositoryStub } = makeSut();
 		const loadSpy = jest.spyOn(loadStudentsRepositoryStub, "load");
 		await sut.load(1, 6);
 		expect(loadSpy).toHaveBeenCalledWith(1, 6);
