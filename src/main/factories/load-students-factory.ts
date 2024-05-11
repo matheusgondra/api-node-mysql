a6f79116e559b0ce03ee659ae0f2363edf38039b
matheusgondra <matheusgondra951@gmail.com>
2024-05-11 13:27:50 -0300

    feat: add new validation in LoadStudentsController factory

--- src/main/factories/load-students-factory.ts
+++ src/main/factories/load-students-factory.ts
@@ -2,12 +2,12 @@ import { DbLoadStudents } from "../../data/use-cases";
 import { StudentRepository } from "../../infra/db/mysql/repositories/student-repository";
 import { LoadStudentsController } from "../../presentation/controllers";
 import { Controller, Validation } from "../../presentation/protocols";
-import { RequiredFieldValidation, ValidationComposite } from "../../validation";
+import { PaginationValidation, ValidationComposite } from "../../validation";
 
 export const makeLoadStudentsController = (): Controller => {
 	const loadStudentsRepository = new StudentRepository();
 	const loadStudents = new DbLoadStudents(loadStudentsRepository);
-	const validations: Validation[] = [];
+	const validations: Validation[] = [new PaginationValidation()];
 	const validation = new ValidationComposite(validations);
 	return new LoadStudentsController(validation, loadStudents);
 };
