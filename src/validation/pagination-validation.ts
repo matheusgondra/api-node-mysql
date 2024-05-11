0ed8a3b5ce9023286d9913474efb94d59b16e3be
matheusgondra <matheusgondra951@gmail.com>
2024-05-11 13:22:10 -0300

    feat: ensure PaginationValidation return InvalidParamError if page is less than 1

--- src/validation/pagination-validation.ts
+++ src/validation/pagination-validation.ts
@@ -4,9 +4,13 @@ import { Validation } from "../presentation/protocols";
 export class PaginationValidation implements Validation {
 	validate(input: any): Error | null {
 		const { page, limit } = input;
-		if (page) {
+		if (page != undefined) {
 			const pageNumber = parseInt(page);
 			if (isNaN(pageNumber)) {
+				return new InvalidParamError("page");
+			}
+
+			if (pageNumber < 1) {
 				return new InvalidParamError("page");
 			}
 		}
