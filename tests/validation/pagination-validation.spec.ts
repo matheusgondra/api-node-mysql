0ac8eb60376ce30656370cb56e435b86323b1c91
matheusgondra <matheusgondra951@gmail.com>
2024-05-11 13:22:31 -0300

    test: ensure PaginationValidation return InvalidParamError if page is less than 1

--- tests/validation/pagination-validation.spec.ts
+++ tests/validation/pagination-validation.spec.ts
@@ -1,10 +1,20 @@
 import { PaginationValidation } from "../../src/validation";
 import { InvalidParamError } from "../../src/presentation/errors";
 
+const makeSut = (): PaginationValidation => {
+	return new PaginationValidation();
+};
+
 describe("PaginationValidation", () => {
 	it("Should return InvalidParamError if page is not a number", () => {
-		const sut = new PaginationValidation();
+		const sut = makeSut();
 		const error = sut.validate({ page: "invalid_number", limit: 6 });
+		expect(error).toEqual(new InvalidParamError("page"));
+	});
+
+	it("Should return InvalidParamError if page is less than 1", () => {
+		const sut = makeSut();
+		const error = sut.validate({ page: 0, limit: 6 });
 		expect(error).toEqual(new InvalidParamError("page"));
 	});
 });
