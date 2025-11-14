b8fcfe5d24fe3ff44f5d066a868b053cc950bdf3
matheusgondra <matheusgondra951@gmail.com>
2025-11-14 15:12:59 -0300

    compilação com swc

--- src/config/customApp.ts
+++ src/config/customApp.ts
@@ -1,20 +1,17 @@
 import express from "express";
 import routes from "../routes/index.js";
 import cors from "cors";
-import pino from "pino-http"
+import { pinoHttp } from "pino-http";
+import { logger } from "../utils/logger.js";
 
 export default () => {
    const app = express();
 
    app.use(
       express.json(),
-      pino({
-         transport: process.env.NODE_ENV !== "production" ? { target: "pino-pretty" } : undefined,
-         name: "api",
-         level: process.env.NODE_ENV !== "production" ? "debug" : "info"
-      }),
+      pinoHttp({ logger }),
       ...routes,
-		cors(),
+      cors(),
    );
 
    return app;
