a83647c6ede2b8dfa7ff9f99901cc921aef33abf
matheusgondra <matheusgondra951@gmail.com>
2025-11-17 15:16:12 -0300

    conexão mysql via uri

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -4,10 +4,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const connection = mysql2.createConnection({
-   host: process.env.DB_HOST,
-   user: process.env.DB_USER,
-   password: process.env.DB_PASSWORD,
-   database: process.env.DB_NAME
+   uri: process.env.DATABASE_URL
 });
 
 export default connection;
